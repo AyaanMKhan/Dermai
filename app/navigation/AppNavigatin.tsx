19a2fa4760708a20020b920ce340dfa41c09e632
Ayaan Khan <140417723+AyaanMKhan@users.noreply.github.com>
2024-12-10 15:37:36 -0500

    Create AppNavigatin.tsx

---
+++ app/navigation/AppNavigatin.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+import LoginScreen from '../screens/LoginScreen';
+import HomeScreen from '../screens/HomeScreen';
+import AboutScreen from '../screens/AboutScreen';
+import GalleryScreen from '../screens/GalleryScreen';
+import ResultsScreen from '../screens/ResultsScreen';
+import NotFoundScreen from '../screens/NotFoundScreen';
+import RegisterScreen from '../screens/RegisterScreen';
+import CameraScreen from '../components/CameraCapture';
+
+const Stack = createStackNavigator();
+
+const AppNavigator: React.FC = () => {
+  return (
+    <Stack.Navigator initialRouteName="Login">
+      <Stack.Screen name="Login" component={LoginScreen} options={{ headerShown: false }}/>
+      <Stack.Screen name="Home" component={HomeScreen} options={{ headerShown: false }}/>
+      <Stack.Screen name="About" component={AboutScreen} options={{ headerShown: false }}/>
+      <Stack.Screen name="Gallery" component={GalleryScreen} options={{ headerShown: false }}/>
+      <Stack.Screen name="Results" component={ResultsScreen} options={{ headerShown: false }}/>
+      <Stack.Screen name="NotFound" component={NotFoundScreen} options={{ headerShown: false }}/>
+      <Stack.Screen name="Register" component={RegisterScreen} options={{ headerShown: false }}/>
+      <Stack.Screen name="Camera" component={CameraScreen} options={{ headerShown: false }}/>
+    </Stack.Navigator>
+  );
+};
+
+export default AppNavigator;
